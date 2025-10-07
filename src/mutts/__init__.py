727d086863ca7c0b7e07f7b9710e37c0f38b03bc
Sujay Patil <sujaysanjeev.patil@gmail.com>
2025-10-06 17:26:51 -0700

    remove __version__ from __init__.py

--- src/mutts/__init__.py
+++ src/mutts/__init__.py
@@ -1,7 +1,5 @@
 """MUTTs - Metadata for User facility Template Transformations"""
 
-__version__ = "0.1.0"
-
 from mutts.retriever import MetadataRetriever
 from mutts.spreadsheet import SpreadsheetCreator
 
